a7a70f8afa93e75a6d4add11ecf98498f29f3993
Leonardo Guilarte <leomiguel1907@gmail.com>
2021-02-14 18:08:10 -0400

    Inicio del proyecto

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">
+    <h2>Inicio de la prueba</h2>
+
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/> -->
+  </div>
+</template>
+
+<style lang="scss">
+@import "assets/css/css.scss";
+</style>
